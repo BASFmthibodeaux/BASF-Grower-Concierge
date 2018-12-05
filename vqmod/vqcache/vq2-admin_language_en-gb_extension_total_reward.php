6da76c9aee8a4468b99e7c543407a3af64ed4a1b
avash_stha <avash@linxsystems.com>
2018-12-05 08:18:03 -0600

    vqmod edit for featured products

---
+++ vqmod/vqcache/vq2-admin_language_en-gb_extension_total_reward.php
@@ -0,0 +1,28 @@
+<?php
+// Heading
+
+            $_['heading_title']    = 'BASF Dollars';
+
+            // Text
+            $_['text_extension']   = 'Extensions';
+            $_['text_success']     = 'Success: You have modified BASF dollars total!';
+            $_['text_edit']        = 'Edit BASF Dollars Total';
+
+            // Entry
+            $_['entry_status']     = 'Status';
+            $_['entry_sort_order'] = 'Sort Order';
+
+            // Error
+            $_['error_permission'] = 'Warning: You do not have permission to modify BASF dollars total!';
+            
+
+
+
+
+
+
+
+
+
+
+
---
+++ vqmod/vqcache/vq2-catalog_language_en-gb_extension_total_reward.php
@@ -0,0 +1,30 @@
+<?php
+// Heading
+
+            $_['heading_title'] = 'Use BASF Dollars (Available %s)';
+
+            // Text
+            $_['text_reward']   = 'BASF Dollars (%s)';
+            $_['text_order_id'] = 'Order ID: #%s';
+            $_['text_success']  = 'Success: Your BASF Dollars discount has been applied!';
+
+            // Entry
+            $_['entry_reward']  = 'BASF Dollars to use (Max %s)';
+
+            // Error
+            $_['error_reward']  = 'Warning: Please enter the amount of BASF Dollars to use!';
+            $_['error_points']  = 'Warning: You don\'t have %s BASF Dollars!';
+            $_['error_maximum'] = 'Warning: The maximum number of BASF Dollars that can be applied is %s!';
+            
+
+
+
+
+
+
+
+
+
+
+
+
